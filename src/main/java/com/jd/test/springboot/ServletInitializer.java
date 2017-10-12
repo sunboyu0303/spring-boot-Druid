d8cc6cac2002abfcd6b297701d9c0abc0f2f0658
sunboyu <sunboyu@jd.com>
2017-10-12 15:49:18 +0800

    spring boot druid

---
+++ src/main/java/com/jd/test/springboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jd.test.springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootDruidApplication.class);
+	}
+
+}
